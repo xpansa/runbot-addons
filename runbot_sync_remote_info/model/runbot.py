4764a47069045836010afb5b7e4526873713f6ac
moylop260 <moylop260@vauxoo.com>
2015-05-15 17:25:37 -0500

    [REF] runbot_sync_remote_info: Fix flake8 and pylint

--- runbot_sync_remote_info/model/runbot.py
+++ runbot_sync_remote_info/model/runbot.py
@@ -17,29 +17,32 @@ class RunbotBranch(models.Model):
 
     def write(self, cr, uid, ids, vals, context=None):
         if 'active' in vals:
-            if vals['active'] == False:
+            if vals['active'] is False:
                 rb_pool = self.pool.get('runbot.build')
                 rbl_pool = self.pool.get('runbot.build.line')
                 build_ids = []
                 if rbl_pool:
                     rbls = rbl_pool.search_read(cr, uid, [
                         ('branch_id', 'in', ids),
-                    ], [('build_id'),])
+                    ], [('build_id')])
                     build_ids = [
                         rbl.get('build_id')[0] for rbl in rbls
                     ]
                 else:
-                    rb = rb_pool.search(cr, uid, [
+                    rb_list = rb_pool.search(cr, uid, [
                         ('branch_id', 'in', ids),
                     ], context=context)
-                    build_ids = [rb.id for rb in rb]
+                    build_ids = [rb.id for rb in rb_list]
                 build_ids = rb_pool.search(cr, uid, [
                     ('state', '<>', 'done'),
                     ('id', 'in', build_ids),
                 ], context=context)
-                rb_pool.kill(cr, uid, build_ids, result='Inactive branch', context=context)
+                rb_pool.kill(
+                    cr, uid, build_ids,
+                    result='Inactive branch',
+                    context=context)
         return super(RunbotBranch, self).write(
-                cr, uid, ids, vals, context=context)
+            cr, uid, ids, vals, context=context)
 
 
 class RunbotRepo(models.Model):
@@ -47,15 +50,8 @@ class RunbotRepo(models.Model):
 
     @api.multi
     def sync_remote(self):
-        _logger.debug('sync remote', self.name)
+        _logger.debug('sync remote %s', self.name)
         if self.host_driver == 'github':
-            github_filter = {
-                'state': 'all',
-            }
-            github_filter_list = [
-                key + '=' + github_filter[key]
-                for key in github_filter.keys()
-            ]
             branches = self.env['runbot.branch'].search([
                 ('repo_id', '=', self.ids),
                 ('name', 'like', 'refs/pull/%')
@@ -71,10 +67,9 @@ class RunbotRepo(models.Model):
                         'branch_remote_state': info_pull['state'],
                         'active': info_pull['state'] == 'open',
                     })
- 
+
     @api.model
     def sync_remote_cron(self):
         repos = self.search([('active', '=', True)])
         for repo in repos:
             repo.sync_remote()
-
